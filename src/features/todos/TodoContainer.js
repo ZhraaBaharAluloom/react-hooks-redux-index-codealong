892947a45ddc05c7787bd5ad4dbef6ab64b12fdc
Zahraa <alwarda@hotmail.co.uk>
2021-08-30 11:57:13 -0700

    display the items with redux

--- src/features/todos/TodoContainer.js
+++ src/features/todos/TodoContainer.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function TodoContainer() {
-  return <div>TodoContainer</div>;
+  const toDoList = useSelector((state) => state.todos.entities).map(
+    (todo, index) => <li key={index}>{todo}</li>
+  );
+
+  return <ul>{toDoList}</ul>;
 }
 
 export default TodoContainer;
